bf596b9d15578bad235430bbb6acb8fc7cd376df
Haley Kloss <hrk85@nau.edu>
2025-09-19 23:54:27 -0700

    set up navigation

--- index.js
+++ index.js
@@ -2,3 +2,25 @@
 import express from "express";
 import bodyParser from "body-parser";
 import axios from "axios";
+
+//set up express, body-parser, and the port
+const app = express();
+const port = 3000;
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+//render home page render
+app.get("/", (req, res) => {
+  res.render("index.ejs", {});
+});
+
+//render search cocktails page
+app.get("/search", (req, res) => {
+  res.render("search.ejs", {});
+});
+
+//if home button is clicked, redirect to home page
+app.get("/clickHome", (req, res) => {
+  return res.redirect('/');
+});
